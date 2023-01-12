f8d200ea9630957495b006db2baa9e3aec1b632f
Antoni Fathalla <antoni.fathalla@learnet.se>
2023-01-12 10:25:32 +0100

    hej

---
+++ project1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class project1 {
+   
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("välkommen till Tensta fight");
+        System.out.print("Välj ett namn: ");
+        String Username=scan.nextLine();
+        System.out.println("välkommen till tensta " +Username);
+
+    }
+}
